1ad9350b72af85dd1bbfd81ed8e4bb4025fa50ba
CarlGriffiths <carl.griffiths504@gmail.com>
2019-10-28 20:12:39 +0000

    added some unit tests

--- BMIUnitTestProject/UnitTest1.cs
+++ BMIUnitTestProject/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using BMICalculator;
+using System;
 
 namespace BMIUnitTestProject
 {
@@ -8,10 +9,32 @@ namespace BMIUnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBMICategoryNormal()
         {
             BMI bmi = new BMI() { WeightStones = 12, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
             Assert.AreEqual(bmi.BMICategory, BMICategory.Normal);
         }
+
+        [TestMethod]
+        public void TestBMICategoryUnderweight()
+        {
+            BMI bmi = new BMI() { WeightStones = 5, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Underweight);
+        }
+
+        [TestMethod]
+        public void TestBMICategoryObese()
+        {
+            BMI bmi = new BMI() { WeightStones = 40, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Obese);
+        }
+
+         [TestMethod]
+        public void TestBMICategoryOverweight()
+        {
+            BMI bmi = new BMI() { WeightStones = 14, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Overweight);
+        }
+    
     }
 }
