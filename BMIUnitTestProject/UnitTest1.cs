b1bae3f9c7eb53538743b6f768f1b9c001f0af90
CarlGriffiths <carl.griffiths504@gmail.com>
2019-11-11 18:22:58 +0000

    adding failed unit test for testing ci pipeline

--- BMIUnitTestProject/UnitTest1.cs
+++ BMIUnitTestProject/UnitTest1.cs
@@ -35,5 +35,11 @@ namespace BMIUnitTestProject
             BMI bmi = new BMI() { WeightStones = 14, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
             Assert.AreEqual(bmi.BMICategory, BMICategory.Overweight);
         }
+        [TestMethod]
+        public void TestBMICategoryOverweight2()
+        {
+            BMI bmi = new BMI() { WeightStones = 14, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Underweight);
+        }
     }
 }
